88ede26afb325cfa6e5045809bca608e7465d16d
LukasRoeding <goskodovesche@gmail.com>
2025-06-21 16:45:29 +0200

    basic scoring and export to web

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,9 @@
 extends Area2D
+@onready var score: Label = $"../../CanvasLayer/Score"
 
 
 func _on_body_entered(body: Node2D) -> void:
 	print("+1 coin")
+	score.add_score()
 	queue_free()
 	pass # Replace with function body.
